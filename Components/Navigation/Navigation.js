529cdffe38ef29da07ffa98141b99b2d2eb9c2d5
emelinelz <emeline.lentz.schmidt@gmail.com>
2019-06-06 12:09:49 +0200

    deuxieme reso conflits emeline

--- Components/Navigation/Navigation.js
+++ Components/Navigation/Navigation.js
@@ -1,11 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import {Button,createBottomTabNavigator, createAppContainer, createStackNavigator, createDrawerNavigator, DrawerItems, } from 'react-navigation';
 import {Platform} from 'react-native';
-=======
+
 import {createBottomTabNavigator, createAppContainer, createStackNavigator, createDrawerNavigator, DrawerItems, } from 'react-navigation';
 import {Platform, StyleSheet} from 'react-native'
->>>>>>> addpages
+
 import Home from '../Screen/Home';
 import SearchScreen from '../Screen/SearchScreen';
 import AddPromenade from '../Screen/AddPromenade';
@@ -37,80 +37,14 @@ var iconAccount = {
 };
 
 
-<<<<<<< HEAD
-var MainNavigator = createBottomTabNavigator(
-  {
-    'Ajouter une promenade': AddPromenade,
-    'Trouver une promenade':SearchScreen
-  },
-  {
-      defaultNavigationOptions: ({ navigation }) => ({
-        tabBarIcon: ({ focused, tintColor, horizontal }) =>{
-          if (navigation.state.routeName == 'Ajouter une promenade'){
-            return <Icon  name="add-circle" />
-          }
-          else if (navigation.state.routeName == 'Trouver une promenade'){
-            return <Icon name="locate" />
-          }
-        }
-      }),
-      tabBarOptions: {
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      },
-    }
-);
-
-
-const DrawerNavigator = createDrawerNavigator(
-  {
-    SearchScreen: SearchScreen,
-    ListScreen: ListScreen,
-    AddPromenade: AddPromenade
-  },
-);
-=======
 
 
 
->>>>>>> addpages
 
 
 var StackNavigator = createStackNavigator({
 
 
-<<<<<<< HEAD
-  Home: Home,
-  SearchScreen: SearchScreen,
-  ListScreen: ListScreen,
-  AddPromenade: AddPromenade,
-  Signup: Signup,
-
-  MainNavigator:{
-    screen: MainNavigator
-  },
-
-  Signin: Signin,
-  CameraScreen: CameraScreen,
-MonCompte:MonCompte,
-PromenadeScreen:PromenadeScreen,
-Promenade:Promenade,
-MesPromenades:MesPromenades,
-maptest:maptest
-},
-{
-  defaultNavigationOptions: {
-    title: 'DoGo',
-    headerStyle: { height: 60 },
-    
-    headerTitleStyle: {
-      flex: 1,
-      textAlign: 'center',
-      color: "#2bcbba",
-      fontWeight: '800'
-    }
-  }
-=======
    Home: Home,
    SearchScreen: SearchScreen,
    ListScreen: ListScreen,
@@ -129,7 +63,6 @@ maptest:maptest
    Signin: Signin,
    CameraScreen: CameraScreen,
 
->>>>>>> addpages
 }
 
 );
